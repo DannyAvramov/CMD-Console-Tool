923400790cb34fac7ae429ea6d0ca28e16c27023
Danny Avramov <dannyavramovski@gmail.com>
2018-07-01 23:00:08 -0400

    Add files via upload

---
+++ C/console.c
@@ -0,0 +1,199 @@
+#include "console.h"
+
+void pause(void) {
+	printf("\n\n> Press any key to continue. . .");
+	while (_kbhit()) {
+		_getch(); 		// Empty the input buffer
+	}
+	_getch(); 			// Wait for a key
+	while (_kbhit()) {
+		_getch(); 		// Empty the input buffer (some keys sends two messages)
+	}
+	return;
+}
+///
+void clear(void) {
+	HANDLE outHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+	DWORD count;
+	DWORD cellCount;
+
+	COORD coord = {
+		0,
+		0
+	};
+
+	if (outHandle == INVALID_HANDLE_VALUE) {
+		return;
+	}
+
+	if (!GetConsoleScreenBufferInfo(outHandle, &bufferInfo)) { 										// Get the number of cells in the current buffer
+		return;
+	}
+
+	cellCount = bufferInfo.dwSize.X * bufferInfo.dwSize.Y;
+
+	if (!FillConsoleOutputCharacter(outHandle, (TCHAR) ' ', cellCount, coord, &count)) { 			// Fill the entire buffer with spaces
+		return;
+	}
+
+	if (!FillConsoleOutputAttribute(outHandle, bufferInfo.wAttributes, cellCount, coord, &count)) { // Fill the entire buffer with the current colors and attributes
+		return;
+	}
+
+	SetConsoleCursorPosition(outHandle, coord); 													// Move the cursor home
+	return;
+}
+///
+void setTitle(const char *TITLE) {
+	TCHAR consoleTitle[256];
+	wsprintfA(consoleTitle, TITLE);
+	SetConsoleTitleA(consoleTitle);
+	return;
+}
+///
+void __stdcall setConsoleColors(const unsigned short ATTRIBS) {
+	CONSOLE_SCREEN_BUFFER_INFOEX cbi;
+	cbi.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfoEx(handle, &cbi);
+	cbi.wAttributes = ATTRIBS;
+	SetConsoleScreenBufferInfoEx(handle, &cbi);
+	return;
+}
+///
+void placeCursor(const unsigned short HORIZONTAL, const unsigned short VERTICAL) {
+	COORD coord;
+	coord.X = HORIZONTAL;
+	coord.Y = VERTICAL;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+	return;
+}
+///
+void setWindow(const LPTSTR TITLE, const USHORT WIDTH, const USHORT HEIGHT, bool bEnableExit, bool bEnableMaximize, bool bEnableMinimize, bool bEnableScrollbar, bool bEnableResizing, bool bEnableFullscreen) {
+	/// Console Title
+	TCHAR consoleTitle[256];
+	wsprintfA(consoleTitle, TITLE);
+	SetConsoleTitleA(consoleTitle);
+	/// Console Dimensions
+	HANDLE outHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (outHandle == INVALID_HANDLE_VALUE) {
+		perror("Unable to get stdout outHandle.");
+		COORD coord = GetLargestConsoleWindowSize(outHandle);
+		if (WIDTH > coord.X) {
+			perror("The width (X) dimension is too large.");
+		}else if (HEIGHT > coord.Y) {
+			perror("The height (Y) dimension is too large.");
+		};
+	}
+
+	CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+
+	if (!GetConsoleScreenBufferInfo(outHandle, &bufferInfo)) {
+		perror("Unable to retrieve screen buffer info.");
+	}
+
+
+	SMALL_RECT winInfo = bufferInfo.srWindow;
+
+	COORD coordWindowSize = {
+		winInfo.Right  - winInfo.Left + 1,
+		winInfo.Bottom - winInfo.Top + 1
+	};
+
+	if (coordWindowSize.X > WIDTH || coordWindowSize.Y > HEIGHT) {
+		SMALL_RECT info = {
+			0,
+			0,
+			WIDTH  < coordWindowSize.X ? WIDTH  - 1 : coordWindowSize.X - 1,
+			HEIGHT < coordWindowSize.Y ? HEIGHT - 1 : coordWindowSize.Y - 1
+		};
+
+		if (!SetConsoleWindowInfo(outHandle, TRUE, &info)) {
+			perror("Unable to resize window before resizing buffer.");
+		};
+	}
+
+	COORD coordSize = {
+		WIDTH,
+		HEIGHT
+	};
+
+	if (!SetConsoleScreenBufferSize(outHandle, coordSize)) {
+		perror("Unable to resize screen buffer.");
+	}
+
+	SMALL_RECT info = {
+		0,
+		0,
+		WIDTH  - 1,
+		HEIGHT - 1
+	};
+
+	if (!SetConsoleWindowInfo(outHandle, TRUE, &info)) {
+		perror("Unable to resize window after resizing buffer.");
+	}
+	/// Window Buttons
+	HWND consoleWindow = GetConsoleWindow();
+	LONG Style = GetWindowLongA(consoleWindow, GWL_STYLE);
+
+	if (!bEnableExit) {
+		HMENU hmenu = GetSystemMenu(consoleWindow, FALSE);
+		EnableMenuItem(hmenu, SC_CLOSE, MF_GRAYED); // Use "MF_ENABLED" to enable close button
+	}
+
+	if (!bEnableMaximize) {
+		Style = Style & ~(WS_MAXIMIZEBOX);
+		SetWindowLongA(consoleWindow, GWL_STYLE, Style);
+	}
+
+	if (!bEnableMinimize) {
+		Style = Style & ~(WS_MINIMIZEBOX);
+		SetWindowLongA(consoleWindow, GWL_STYLE, Style);
+	}
+	/// Window Characteristics
+	if (!bEnableResizing) {
+		Style = Style & ~(WS_THICKFRAME);
+		SetWindowLongA(consoleWindow, GWL_STYLE, Style);
+	}
+
+	if (bEnableScrollbar) {
+		GetConsoleScreenBufferInfo(outHandle, &bufferInfo);
+		COORD Size = {
+			bufferInfo.srWindow.Right - bufferInfo.srWindow.Left + 1,
+			bufferInfo.srWindow.Bottom - bufferInfo.srWindow.Top + 1
+		};
+		SetConsoleScreenBufferSize(outHandle, Size);
+		ShowScrollBar(consoleWindow, 0, false);
+	}
+
+	if (bEnableFullscreen) {
+		SendMessageA(GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, MNS_DRAGDROP);
+	}
+	return;
+}
+///
+void setWindowSize(const unsigned short WIDTH, const unsigned short HEIGHT) {
+	COORD coord;
+	coord.X = WIDTH;
+	coord.Y = HEIGHT;
+	SMALL_RECT Rect;
+	Rect.Top = 0;
+	Rect.Left = 0;
+	Rect.Bottom = HEIGHT - 1;
+	Rect.Right = WIDTH - 1;
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coord);	// Set Buffer Size 
+	SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), TRUE, &Rect);	// Set Window Size 
+	return;
+}
+///
+void setColor(const unsigned short COLOR) {
+	WORD wColor;
+	CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &bufferInfo)) { 			// Use csbi for the wAttributes word											
+		wColor = (bufferInfo.wAttributes & 0xF0) + (COLOR & 0x0F);  // Mask out all but the background attribute, and add in the foreground color
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
+	}
+	return;
+}
---
+++ C/console.h
@@ -0,0 +1,169 @@
+/*
+//////////////////////////////////////////////
+// Created 	By: Danny Avramov	      		//
+// Description: Created for console use.	//
+// 		  		Control sizing and enable   //
+//		  		disabling window functions. //
+//////////////////////////////////////////////
+*/
+#ifndef CONSOLE_H
+#define CONSOLE_H
+#pragma comment(lib, "user32") // For Full-screen
+#ifndef _INC_STDIO
+	#include <stdio.h>
+#endif
+#ifndef _STDBOOL
+	#include <stdbool.h>
+#endif
+#ifndef _INC_CONIO
+	#include <conio.h>
+#endif
+#ifndef _WINDOWS_H
+	#include <Windows.h>
+#endif
+
+void pause(void);
+/*
+///////////////////////////////////////////////////////////////
+// Documentation:                                            //
+// - Equivalent to typing "pause" into command prompt. (cmd) //
+// - Pauses the console until a random key is pressed.       //
+///////////////////////////////////////////////////////////////
+*/
+
+
+void clear(void);
+/*
+/////////////////////////////////////////////////////////////
+// Documentation:										   //
+// - Equivalent to typing "cls" into command prompt. (cmd) //
+// - Clears the console screen but issuing a new buffer.   //
+/////////////////////////////////////////////////////////////
+*/
+
+
+void setTitle(const char *TITLE);
+/*
+/////////////////////////////////////////////////////////////////////////
+// Documentation:												       //
+// - Equivalent to typing "title <example>" into command prompt. (cmd) //
+// - Sets a title for the window of the console.                       //
+// Example:															   //
+// > setTitle("I like big butts");									   //
+/////////////////////////////////////////////////////////////////////////
+*/
+
+
+void WINAPI setConsoleColors(const unsigned short ATTRIBS);
+/*
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Documentation:																							 //
+// - Equivalent to typing "color <example>" into command prompt. (cmd)										 //
+// - Responsible for changing the console foreground color (characters) and the background color.			 //
+// Example:																									 //
+// > setConsoleColors(BACKGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY);								 //
+// Description of Example:																					 //
+// - Sets the background color of the console towards blue, the foreground color towards red				 //
+//   and makes the foreground color opacity to the max.														 //
+// - The function's params can use bit-shifting operators to set the background and foreground separately.	 //
+// Example:																									 //
+// > setConsoleColors(White);                                                                                //
+// Description of Example:																					 //
+// - The color enum members can be used to set the foreground of the console leaving the background default. //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+void placeCursor(const unsigned short HORIZONTAL, const unsigned short VERTICAL);
+/*
+//////////////////////////////////////////////////////////////////////////////////////
+// Documentation:																	//
+// - Moves the placement of a character(s) or printed character(s)					//
+//   to the coordinates assigned in the function's params.							//
+// - Can use to move or print text anywhere on the screen.							//
+// Example:																			//
+// > placeCursor(25, 50);															//
+// Description of Example:															//
+// - Moves the character new position 25 spaces to the left and 50 spaces downward. //
+//////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+void setWindow(const LPTSTR TITLE, const USHORT WIDTH, const USHORT HEIGHT, bool bEnableExit, bool bEnableMaximize, bool bEnableMinimize, bool bEnableScrollbar, bool bEnableResizing, bool bEnableFullscreen);
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Documentation:																						  //
+// - Uses all the console assigning functions to define the window with the characteristics				  //
+//   of the user's input determine but the function's params.											  //
+// - Can set title to window, change window size, toggle scrollbar,										  //
+//   toggle full-screen and toggle window state buttons.												  //
+// Example:																								  //
+// > setWindow("My Title" 32, 64, false, true, true, true, true, false);								  //
+// Description of Example:																				  //
+// - Sets the title to "My Title", makes the window 32 spaces in width and 64 spaces in height,			  //
+// - Disables close button, Enables the maximize buttons, Enables the minimize button,					  //
+// - Enables the resizing ability, Toggles scroll bar to true & Toggles the full-screen ability to false. //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+void setWindowSize(const unsigned short WIDTH, const unsigned short HEIGHT);
+/*
+/////////////////////////////////////////////////////////////////////////////////////////
+// Documentation:																	   //
+// - Equivalent to typing "mode con:cols=<example>" and "mode con:lines=<example>"	   //
+//   into command prompt. (cmd)														   //
+// - The params of the function define the window's width and height.                  //
+// Example:																			   //
+// > setWindowSize(100, 200);														   //
+// Description of Example:															   //
+// - Assigns the width of the window to be 100 spaces and the height to be 200 spaces. //
+/////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+void setColor(const unsigned short COLOR);
+/*
+/////////////////////////////////////////////////////////////////////////
+// Documentation:                                                      //
+// - Equivalent to typing "color <example>" into command prompt. (cmd) //
+// - Function is responsible for changing the foreground color		   //
+//   (the character colors) of the console.							   //
+// Example:															   //
+// > setColor(Blue);												   //
+/////////////////////////////////////////////////////////////////////////
+*/
+
+enum Color {
+	Black			= 0,
+	Dark_Blue		= 1,
+	Dark_Green		= 2,
+	Dark_Turquise	= 3,
+	Dark_Red		= 4,
+	Dark_Purple		= 5,
+	Dark_Yellow		= 6,
+	Gray			= 7,
+	Dark_Gray		= 8,
+	Blue			= 9,
+	Green			= 10,
+	Turquise		= 11,
+	Red				= 12,
+	Purple			= 13,
+	Yellow			= 14,
+	White			= 15
+};
+/*
+///////////////////////////////////////////////////////////////////////////////////
+// Documentation:																 //
+// - An enumeration of color-coded numbers to be used by						 //
+//   the two console color defining functions setConsoleColors() and setColor(), //
+//   also can use the enum members as params for the functions listed above.	 //
+// Example:																		 //
+// > setColor(Green);                                                            //
+// Description of Example:														 //
+// - The enum member which holds the color-code integer of green				 //
+//   is being used to set the foreground of the console.						 //
+///////////////////////////////////////////////////////////////////////////////////
+*/
+
+#endif // CONSOLE_H
